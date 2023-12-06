fec84104b2fe3109ba4bac58c4bbafccf0f7b05c
ZeeiD <zeeideveloper19@gmail.com>
2023-12-06 21:46:38 +0700

    update web v1

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 // app.js
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const checker = require('./checker');
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
+    res.render('index', { result: null });
+});
+
+app.post('/', (req, res) => {
+    const { socksDetails, socksUsername, socksPassword } = req.body;
+
+    // Parse socksDetails into hostname and port
+    const [hostname, port] = socksDetails.split(':');
+
+    // Set proxy details in checker.js
+    checker.setProxyDetails(hostname, parseInt(port), socksUsername, socksPassword);
+
+    // Perform proxy check using checker.js
     const result = checker.checkProxy();
+
+    // Render the result on the webpage
     res.render('index', { result });
 });
 
--- checker.js
+++ checker.js
@@ -20,13 +20,26 @@ if (os.platform() == 'win32') {
     var chilkat = require('@chilkat/ck-node20-macosx');
 }
 
+let proxyDetails = {};
+
+function setProxyDetails(hostname, port, username, password) {
+    proxyDetails = {
+        hostname,
+        port,
+        username,
+        password,
+    };
+}
+
 function checkProxy() {
     var http = new chilkat.Http();
     http.SocksVersion = 5;
-    http.SocksHostname = "syd.socks.ipvanish.com";
-    http.SocksPort = 1080;
-    http.SocksUsername = "G1emUVl36Ed4";
-    http.SocksPassword = "Pq6PT1LY";
+
+    // Use proxy details from user input
+    http.SocksHostname = proxyDetails.hostname;
+    http.SocksPort = proxyDetails.port;
+    http.SocksUsername = proxyDetails.username;
+    http.SocksPassword = proxyDetails.password;
 
     var html = http.QuickGetStr("https://api.ipify.org/?format=json");
     if (http.LastMethodSuccess !== true) {
@@ -37,5 +50,6 @@ function checkProxy() {
 }
 
 module.exports = {
+    setProxyDetails,
     checkProxy,
 };
